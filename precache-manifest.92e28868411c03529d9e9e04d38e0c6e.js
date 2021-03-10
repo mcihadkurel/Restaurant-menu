143dd16af14f65e82c27b70d3b564665845e4c63
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-10 15:23:23 +0100

    Updates

---
+++ precache-manifest.92e28868411c03529d9e9e04d38e0c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b66f1a3dc9060828f0b85b527fc4ad",
+    "url": "/Restaurant-menu/index.html"
+  },
+  {
+    "revision": "e0ace015f2fb781d74ca",
+    "url": "/Restaurant-menu/static/css/main.42be657d.chunk.css"
+  },
+  {
+    "revision": "317cceaa682868a51da0",
+    "url": "/Restaurant-menu/static/js/2.cd2a891f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Restaurant-menu/static/js/2.cd2a891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ace015f2fb781d74ca",
+    "url": "/Restaurant-menu/static/js/main.1fd0c0a5.chunk.js"
+  },
+  {
+    "revision": "ee2d4a973dc7d50b87f9",
+    "url": "/Restaurant-menu/static/js/runtime-main.27ec35ae.js"
+  }
+]);
